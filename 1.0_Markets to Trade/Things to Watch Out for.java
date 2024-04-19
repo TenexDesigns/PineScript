ff0a82457c865c3d6182434f7a0650d8e2ef1a6b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-04-19 12:27:01 +0300

    Create Things to Watch Out for.java

---
+++ 1.0_Markets to Trade/Things to Watch Out for.java
@@ -0,0 +1,3 @@
+News: Many market participants keep tabs on the news in real-time; positive or negative headlines affecting a market can quickly drive supply or demand down
+Central bank policy: Central bank decisions – such as interest rate changes – can have a profound effect on the flow of money around the world, and will have a significant impact on demand
+Government data: Government releases can affect demand. Unemployment information or inflation data, for example, offer insight into the strength of an economy – which may make it more attractive to investors
