e12563ff15b040e4857e19bf5bb9018580779d4b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-04-19 15:28:46 +0300

    Create Volume Indicators AND INDICTORS i WOULD USE.java

---
+++ 1.0_Liquidity Indictors and Imbalce and Pirce Movement/Volume Indicators AND INDICTORS i WOULD USE.java
@@ -0,0 +1,22 @@
+https://www.tradingview.com/scripts/volume/page-6/
+
+
+**********  https://www.tradingview.com/script/nYrL0Dz6-Volume-Trend-Meter-LTF/                                            Volume Trend Meter LTF    ----->   HIS SCRIPTS https://www.tradingview.com/u/dman103/#published-scripts
+*********  https://www.tradingview.com/script/LFBaHNuA-Pivot-Points-Algo/
+********   https://www.tradingview.com/script/0lERSwqB-Rabbit-Hole/
+
+
+
+  
+https://www.tradingview.com/script/GniGSDgt/                                                                             - Relative Volume Indicator
+https://www.tradingview.com/script/esZMDlod-Volume-CVD-and-Open-Interest/                                                 -Volume CVD and Open Interest
+https://www.tradingview.com/script/p4slw9Ze-Combined-Moving-Averages-Squeeze-Volume-Spike-Signals/                         Combined Moving Averages + Squeeze & Volume Spike Signals
+https://www.tradingview.com/script/4oAJY0xk-Volume-Time-of-Day/                                                            Volume+ (Time of Day)
+https://www.tradingview.com/script/BVpLh1fK-Pressure-Volume-by-Molnar/                                                     Pressure Volume by Molnar
+https://www.tradingview.com/script/2LoJi1nq-Buy-and-Sell-Pressure/                                                         Buy and Sell Pressure
+https://www.tradingview.com/script/lkZA06Vy-Buy-Sell-Vol-with-50EMA/                                                       Buy_Sell_Vol_with_50EMA
+https://www.tradingview.com/script/A3MsJPvo-TR-High-Low-meter/                                                             TR High/Low meter
+https://www.tradingview.com/script/nybqwNWI-Price-Trend-Meter/
+https://www.tradingview.com/script/ywEEvEtI-Volume-Trend-Meter/
+
+
