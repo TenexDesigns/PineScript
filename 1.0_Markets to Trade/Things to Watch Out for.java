ee6a9a80a43995de4ffcfa77382a42658687d04f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-04-19 12:30:22 +0300

    Update Things to Watch Out for.java

--- 1.0_Markets to Trade/Things to Watch Out for.java
+++ 1.0_Markets to Trade/Things to Watch Out for.java
@@ -1,3 +1,10 @@
 News: Many market participants keep tabs on the news in real-time; positive or negative headlines affecting a market can quickly drive supply or demand down
 Central bank policy: Central bank decisions – such as interest rate changes – can have a profound effect on the flow of money around the world, and will have a significant impact on demand
 Government data: Government releases can affect demand. Unemployment information or inflation data, for example, offer insight into the strength of an economy – which may make it more attractive to investors
+
+
+  Volatility is how much an asset’s price will swing up and down over a period. Bears and bulls rarely have total control over a market. Instead, its price will oscillate as one group then another takes over.
+  Highly volatile markets see significant moves, which create lots of profit opportunities – but will also bring increased risk
+  VOLUME ---
+
+  
