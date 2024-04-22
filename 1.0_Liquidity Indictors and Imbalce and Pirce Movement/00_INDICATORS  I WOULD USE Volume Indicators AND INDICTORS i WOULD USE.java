a5896bfe41c3be68bf7beefa580699242596565e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-04-22 21:08:06 +0300

    Update 00_INDICATORS  I WOULD USE Volume Indicators AND INDICTORS i WOULD USE.java

--- 1.0_Liquidity Indictors and Imbalce and Pirce Movement/00_INDICATORS  I WOULD USE Volume Indicators AND INDICTORS i WOULD USE.java
+++ 1.0_Liquidity Indictors and Imbalce and Pirce Movement/00_INDICATORS  I WOULD USE Volume Indicators AND INDICTORS i WOULD USE.java
@@ -1,25 +1,25 @@
 https://www.tradingview.com/scripts/volume/page-6/
 
-00000000     https://in.tradingview.com/script/OBnNYa1d-Uber-Baseline-UTS/ --------------------- check it out
-********     https://www.tradingview.com/script/h6IgRxWC-Buy-Sell-Indicator/
-**** https://in.tradingview.com/script/TQSL9xVn-Range-Filter-x-Hull-Suite/
- **** https://in.tradingview.com/script/J8GzFGfD-Range-Filter-Buy-and-Sell-5min-guikroth-version/
- ********      https://br.tradingview.com/script/cAQJtauT-SuperTREX/
- https://br.tradingview.com/script/MX8EUksX/
-**********    https://www.tradingview.com/script/nYrL0Dz6-Volume-Trend-Meter-LTF/                                            Volume Trend Meter LTF    ----->   HIS SCRIPTS https://www.tradingview.com/u/dman103/#published-scripts
-*********     https://www.tradingview.com/script/LFBaHNuA-Pivot-Points-Algo/
-********      https://www.tradingview.com/script/0lERSwqB-Rabbit-Hole/
-*******       https://www.tradingview.com/script/usbHOChM-DMI-Modified-Strategy/
-*******       https://www.tradingview.com/script/WssGr0yH-Adaptive-Donchian-Lines/   ---------------------
-*******       https://www.tradingview.com/script/HOm8KLbF-RSI-Histogram/
-*******       https://www.tradingview.com/script/BSiT7aK1-MA/
-******        https://www.tradingview.com/script/31pQAX56-Close-Counter-Histogram/
-*******       https://www.tradingview.com/script/HHGMSQFp-Price-Volume-Value-Histogram/
-*********     https://www.tradingview.com/script/OULfXjkq-Heikin-Ashi-Trend-Indicator/
-********      https://www.tradingview.com/script/03Alv7bW-On-Balance-Volume-Momentum/
-*******B      https://www.tradingview.com/script/yLP4Vbsy-Heiken-Ashi-Smoothed-Net-Volume/
-*******       https://www.tradingview.com/script/eSVRUwCf-TFS-Volume-Oscillator-Noise-Filtered/
-*********     https://www.tradingview.com/script/wCbldhhg/
+00000000     https://in.tradingview.com/script/OBnNYa1d-Uber-Baseline-UTS/ --------------------- check it out )))))))))))))))))))))))
+********     https://www.tradingview.com/script/h6IgRxWC-Buy-Sell-Indicator/   XXXXXXXXXXXX
+**** https://in.tradingview.com/script/TQSL9xVn-Range-Filter-x-Hull-Suite/   )))))))))))))))))))))))))))))))))
+ **** https://in.tradingview.com/script/J8GzFGfD-Range-Filter-Buy-and-Sell-5min-guikroth-version/  )))))))))))))))))))))))))
+ ********      https://br.tradingview.com/script/cAQJtauT-SuperTREX/  XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ https://br.tradingview.com/script/MX8EUksX/  ---------------------------------------------------------lllllllllllllllll
+**********    https://www.tradingview.com/script/nYrL0Dz6-Volume-Trend-Meter-LTF/        ))))))))))))))))))))))))))                                    Volume Trend Meter LTF    ----->   HIS SCRIPTS https://www.tradingview.com/u/dman103/#published-scripts
+*********     https://www.tradingview.com/script/LFBaHNuA-Pivot-Points-Algo/  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+********      https://www.tradingview.com/script/0lERSwqB-Rabbit-Hole/  )))))))))))))))))))))))))))
+*******       https://www.tradingview.com/script/usbHOChM-DMI-Modified-Strategy/  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+*******       https://www.tradingview.com/script/WssGr0yH-Adaptive-Donchian-Lines/   ---------------------XXXXXXXXXXXXXXXXXXXXXX
+*******       https://www.tradingview.com/script/HOm8KLbF-RSI-Histogram/  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+*******       https://www.tradingview.com/script/BSiT7aK1-MA/  XXXXXXXXXXXXXXXXXXXXXXXXXX
+******        https://www.tradingview.com/script/31pQAX56-Close-Counter-Histogram/  )))))))))))))))))))))))))))))))))))
+*******       https://www.tradingview.com/script/HHGMSQFp-Price-Volume-Value-Histogram/  XXXXXXXXXXXXXXXXXXXXXXXXXXX
+*********     https://www.tradingview.com/script/OULfXjkq-Heikin-Ashi-Trend-Indicator/  ))))))))))))))))))))))))))))
+********      https://www.tradingview.com/script/03Alv7bW-On-Balance-Volume-Momentum/   )))))))))))))))))))))))))))))))))))
+*******B      https://www.tradingview.com/script/yLP4Vbsy-Heiken-Ashi-Smoothed-Net-Volume/  ))))))))))))))))))))))))))))))))))))))0
+*******       https://www.tradingview.com/script/eSVRUwCf-TFS-Volume-Oscillator-Noise-Filtered/ )))))))))))))))))))))))))))))))))))
+*********     https://www.tradingview.com/script/wCbldhhg/  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 ********      https://www.tradingview.com/script/Ihpq7ye5-DsD/
 ********      https://www.tradingview.com/script/ywEEvEtI-Volume-Trend-Meter/
   *****       https://www.tradingview.com/script/qvdqvpYn-Market-Emotions/
