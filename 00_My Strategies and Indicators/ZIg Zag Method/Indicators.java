5a831a605ad16a2d6551c2531064debd10ea53d2
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-04 15:11:30 +0300

    Create Indicators.java

---
+++ 00_My Strategies and Indicators/ZIg Zag Method/Indicators.java
@@ -0,0 +1,6 @@
+Recursive Zigzag [Trendoscope] ---- https://www.tradingview.com/v/J6mxhxdn/
+Multi-Timeframe Recursive Zigzag [Trendoscope®]  ------------  https://www.tradingview.com/v/3rKS4EMJ/
+Recursive Micro Zigzag  ---------------  https://www.tradingview.com/v/WCQ3CFl6/
+3 Zigzag for MTF Fib Alert [MsF] ----------  https://www.tradingview.com/v/RDhgaIHB/
+Multi Level Zigzag  --------------  https://www.tradingview.com/v/p9YFvFjy/
+RSI Swing Indicator v2  ------------  https://www.tradingview.com/script/uHew29m1-RSI-Swing-Indicator-v2/
