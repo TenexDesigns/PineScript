234b0b8feccf2f1036509a7ef922f808c4a92d97
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-04 15:40:43 +0300

    Update Indicators.java

--- 00_My Strategies and Indicators/ZIg Zag Method/Indicators.java
+++ 00_My Strategies and Indicators/ZIg Zag Method/Indicators.java
@@ -1,6 +1,11 @@
+RSI Market Structure ---------   https://www.tradingview.com/v/jqiRniZL/
+[KK] ZigZag Percent Reversal - Forecast - patterns - labels  ------------  https://www.tradingview.com/script/Di6vU1xj-KK-ZigZag-Percent-Reversal-Forecast-patterns-labels/
+  
+
 Recursive Zigzag [Trendoscope] ---- https://www.tradingview.com/v/J6mxhxdn/
 Multi-Timeframe Recursive Zigzag [Trendoscope®]  ------------  https://www.tradingview.com/v/3rKS4EMJ/
 Recursive Micro Zigzag  ---------------  https://www.tradingview.com/v/WCQ3CFl6/
 3 Zigzag for MTF Fib Alert [MsF] ----------  https://www.tradingview.com/v/RDhgaIHB/
 Multi Level Zigzag  --------------  https://www.tradingview.com/v/p9YFvFjy/
 RSI Swing Indicator v2  ------------  https://www.tradingview.com/script/uHew29m1-RSI-Swing-Indicator-v2/
+  RSI Market Structure  --------------- https://www.tradingview.com/script/jqiRniZL-RSI-Market-Structure/
